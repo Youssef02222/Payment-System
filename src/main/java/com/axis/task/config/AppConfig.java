424e1f88df09ecba05a8b12d4ef209780c72a8d3
Youssef03333 <youssef.ibrahim.mail.1@gmail.com>
2024-01-25 01:42:10 +0200

    feat(swagger): add apis operation description

--- src/main/java/com/axis/task/config/AppConfig.java
+++ src/main/java/com/axis/task/config/AppConfig.java
@@ -1,15 +1,9 @@
 package com.axis.task.config;
 
 import org.modelmapper.ModelMapper;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class AppConfig {
@@ -18,22 +12,11 @@ public class AppConfig {
         return new ModelMapper();
     }
     @Bean
-    public Docket docket(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("axis-task")
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.axis.task"))
-                .paths(PathSelectors.any())
+    public GroupedOpenApi controllerApi() {
+        return GroupedOpenApi.builder()
+                .group("controller-api")
+                .packagesToScan("com.axis.task") // Specify the package to scan
                 .build();
     }
-    @Bean
-    public ApiInfo apiInfo(){
-        return new ApiInfoBuilder()
-                .title("Axis Task")
-                .description("Axis Task API")
-                .contact(new Contact("Youssef", "https://www.linkedin.com/in/youssef-ibrahem-dev/","youssef.ibrahim.salama.mail@gmail.com"))
-                .version("1.0")
-                .build();
-    }
+
 }
--- src/main/java/com/axis/task/controller/AccountController.java
+++ src/main/java/com/axis/task/controller/AccountController.java
@@ -6,33 +6,38 @@ import com.axis.task.model.request.UpdateAccountRequest;
 import com.axis.task.model.response.CreateAccountResponse;
 import com.axis.task.model.response.GenericResponse;
 import com.axis.task.service.AccountService;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.security.auth.login.AccountNotFoundException;
 
 @RestController
+@Tag(name = "Accounts APIs", description = "Account management controller")
 public class AccountController {
     private static final String SUCCESS = "SUCCESS";
     @Autowired
     AccountService accountService;
-    @ApiOperation(value = "get Account details", response = AccountEntity.class)
-    @GetMapping("/get-account/{accountId}")
+//    @ApiOperation(value = "get Account details", response = AccountEntity.class)
+@Operation(summary = "get Account details",responses = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "successful operation")})
+@GetMapping("/get-account/{accountId}")
     public GenericResponse<AccountEntity> getAccount(@PathVariable String accountId) {
         return GenericResponse.<AccountEntity>builder().data(accountService.getAccount(accountId)).success(true).message(SUCCESS).build();
     }
-    @ApiOperation(value = "Open new Account", response = CreateAccountResponse.class)
-    @PostMapping("/create-account")
+//    @ApiOperation(value = "Open new Account", response = CreateAccountResponse.class)
+@Operation(summary = "Open new Account")
+@PostMapping("/create-account")
     public GenericResponse<CreateAccountResponse> createAccount(@RequestBody CreateAccountRequest createAccountRequest) {
         return GenericResponse.<CreateAccountResponse>builder().data(accountService.createAccount(createAccountRequest)).success(true).message(SUCCESS).build();
     }
-    @ApiOperation(value = "update Account data", response = AccountEntity.class)
+    @Operation(summary = "update account info")
     @PutMapping("/update-account")
     public GenericResponse<AccountEntity> updateAccount(@RequestBody UpdateAccountRequest updateAccountRequest) {
         return GenericResponse.<AccountEntity>builder().data(accountService.updateAccount(updateAccountRequest)).success(true).message(SUCCESS).build();
     }
-    @ApiOperation(value = "delete Account data")
+    @Operation(summary = "delete Account permanently")
     @DeleteMapping("/delete-account/{accountId}")
     public GenericResponse<String> deleteAccount(@PathVariable String accountId) throws AccountNotFoundException {
         accountService.deleteAccount(accountId);
