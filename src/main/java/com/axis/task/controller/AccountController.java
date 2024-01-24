e931decf8932524da5f677cee6e84893ac476c76
Youssef03333 <youssef.ibrahim.mail.1@gmail.com>
2024-01-25 00:47:02 +0200

    feat(swagger desc): add swagger description to controllers

--- src/main/java/com/axis/task/controller/AccountController.java
+++ src/main/java/com/axis/task/controller/AccountController.java
@@ -6,6 +6,7 @@ import com.axis.task.model.request.UpdateAccountRequest;
 import com.axis.task.model.response.CreateAccountResponse;
 import com.axis.task.model.response.GenericResponse;
 import com.axis.task.service.AccountService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,19 +17,22 @@ public class AccountController {
     private static final String SUCCESS = "SUCCESS";
     @Autowired
     AccountService accountService;
-
+    @ApiOperation(value = "get Account details", response = AccountEntity.class)
     @GetMapping("/get-account/{accountId}")
     public GenericResponse<AccountEntity> getAccount(@PathVariable String accountId) {
         return GenericResponse.<AccountEntity>builder().data(accountService.getAccount(accountId)).success(true).message(SUCCESS).build();
     }
+    @ApiOperation(value = "Open new Account", response = CreateAccountResponse.class)
     @PostMapping("/create-account")
     public GenericResponse<CreateAccountResponse> createAccount(@RequestBody CreateAccountRequest createAccountRequest) {
         return GenericResponse.<CreateAccountResponse>builder().data(accountService.createAccount(createAccountRequest)).success(true).message(SUCCESS).build();
     }
+    @ApiOperation(value = "update Account data", response = AccountEntity.class)
     @PutMapping("/update-account")
     public GenericResponse<AccountEntity> updateAccount(@RequestBody UpdateAccountRequest updateAccountRequest) {
         return GenericResponse.<AccountEntity>builder().data(accountService.updateAccount(updateAccountRequest)).success(true).message(SUCCESS).build();
     }
+    @ApiOperation(value = "delete Account data")
     @DeleteMapping("/delete-account/{accountId}")
     public GenericResponse<String> deleteAccount(@PathVariable String accountId) throws AccountNotFoundException {
         accountService.deleteAccount(accountId);
--- src/main/java/com/axis/task/controller/TransactionController.java
+++ src/main/java/com/axis/task/controller/TransactionController.java
@@ -7,6 +7,7 @@ import com.axis.task.model.response.GenericResponse;
 import com.axis.task.model.response.TransactionResponse;
 import com.axis.task.service.AccountService;
 import com.axis.task.service.TransactionService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,23 +18,27 @@ public class TransactionController {
     TransactionService transactionService;
     @Autowired
     AccountService accountService;
-
+    @ApiOperation(value = "Inquire for a transaction", response = TransactionEntity.class)
     @GetMapping("/get-transaction/{transactionId}")
     public GenericResponse<TransactionEntity> getTransaction(@PathVariable String transactionId) {
         return GenericResponse.<TransactionEntity>builder().data(transactionService.getTransaction(transactionId)).success(true).message(SUCCESS).build();
     }
+    @ApiOperation(value = "get account current balance")
     @GetMapping("/get-balance/{accountId}")
     public GenericResponse<Double> getBalance(@PathVariable String accountId) {
         return GenericResponse.<Double>builder().data(accountService.getBalance(accountId)).success(true).message(SUCCESS).build();
     }
+    @ApiOperation(value = "deposit fund to account", response = TransactionResponse.class)
     @PostMapping("/deposit")
     public GenericResponse<TransactionResponse> deposit(@RequestBody DepositRequest depositRequest) {
         return GenericResponse.<TransactionResponse>builder().data(transactionService.deposit(depositRequest)).success(true).message(SUCCESS).build();
     }
+    @ApiOperation(value = "withdraw fund from account", response = TransactionResponse.class)
     @PostMapping("/withdraw")
     public GenericResponse<TransactionResponse> withdraw(@RequestBody WithdrawRequest withdrawRequest) {
         return GenericResponse.<TransactionResponse>builder().data(transactionService.withdraw(withdrawRequest)).success(true).message(SUCCESS).build();
     }
+    @ApiOperation(value = "reverse a transaction", response = TransactionResponse.class)
     @GetMapping("/reverse-transaction/{transactionId}")
     public GenericResponse<TransactionResponse> reverseTransaction(@PathVariable String transactionId) {
         return GenericResponse.<TransactionResponse>builder().data(transactionService.reverseTransaction(transactionId)).success(true).message(SUCCESS).build();
