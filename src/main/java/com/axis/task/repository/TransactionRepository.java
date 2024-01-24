11c027cdfb1eb8b061a5ad6c2516af0cbe250a01
Youssef03333 <youssef.ibrahim.mail.1@gmail.com>
2024-01-24 23:47:59 +0200

    feat(repository): add functions to repository

--- src/main/java/com/axis/task/repository/TransactionRepository.java
+++ src/main/java/com/axis/task/repository/TransactionRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<TransactionEntity, Long> {
-
+    TransactionEntity findByTransactionId(String transactionId);
 }
