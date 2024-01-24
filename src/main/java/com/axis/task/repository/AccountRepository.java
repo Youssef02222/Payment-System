bb20ca0fbac24fa62f89dc32881613c1879b880f
Youssef03333 <youssef.ibrahim.mail.1@gmail.com>
2024-01-24 21:49:56 +0200

    feat(repository): add Account & Transaction repositories

---
+++ src/main/java/com/axis/task/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.axis.task.repository;
+
+import com.axis.task.entity.AccountEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
+
+}
---
+++ src/main/java/com/axis/task/repository/TransactionRepository.java
@@ -0,0 +1,10 @@
+package com.axis.task.repository;
+
+import com.axis.task.entity.TransactionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<TransactionEntity, Long> {
+
+}
